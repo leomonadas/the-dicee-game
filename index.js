6d4c479485d35b1a188c3253f895385d9c8e7fe0
Leonardo <leonardokennedyalvesdelima@gmail.com>
2021-12-29 18:47:06 -0300

    atualização no h1

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 
 function dicee() {
-    
+        var title = document.getElementById('title');
         var imgOne = document.querySelector(".img1");
         var imgTwo = document.querySelector(".img2");
         var randomNumberOne = Math.floor((Math.random()*6)) + 1;
         var randomNumberTwo = Math.floor((Math.random()*6)) + 1;
+
+        if (randomNumberOne > randomNumberTwo) {
+            title.innerHTML = 'Player One Won!';
+        }
+        else if (randomNumberOne < randomNumberTwo) {
+            title.innerHTML = 'Player Two Won!';
+        }
+        else {
+            title.innerHTML = 'Draw!';
+        }
 
         if (randomNumberOne == 1) {
             imgOne.src = 'images/dice1.png';
@@ -48,3 +58,33 @@ function dicee() {
 }
 
 
+// Resposta do desafio pela Prof Angela
+
+// var randomNumber1 = Math.floor(Math.random() * 6) + 1; //1-6
+
+// var randomDiceImage = "dice" + randomNumber1 + ".png"; //dice1.png - dice6.png
+
+// var randomImageSource = "images/" + randomDiceImage; //images/dice1.png - images/dice6.png
+
+// var image1 = document.querySelectorAll("img")[0];
+
+// image1.setAttribute("src", randomImageSource);
+
+
+// var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+
+// var randomImageSource2 = "images/dice" + randomNumber2 + ".png";
+
+// document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
+
+
+// //If player 1 wins
+// if (randomNumber1 > randomNumber2) {
+//   document.querySelector("h1").innerHTML = "🚩 Play 1 Wins!";
+// }
+// else if (randomNumber2 > randomNumber1) {
+//   document.querySelector("h1").innerHTML = "Player 2 Wins! 🚩";
+// }
+// else {
+//   document.querySelector("h1").innerHTML = "Draw!";
+// }
